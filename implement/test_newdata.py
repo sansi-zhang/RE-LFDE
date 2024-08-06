f023a90dc4c9a62abc14ffa292c740844b96754a
zhangsansi <85163357+sansi-zhang@users.noreply.github.com>
2024-08-06 16:45:43 +0800

    Update test_newdata.py

--- implement/test_newdata.py
+++ implement/test_newdata.py
@@ -5,19 +5,9 @@ from torchvision.transforms import ToTensor
 
 
 from model.model_Quant import Net_Quant
-from model.model_Quant2 import Net_Quant2
-from model.model_Quant_8bit import Net_Quant_8bit
-from model.model_Quant_w2bit import Net_Quant_w2bit
-
-
 from model.model_Full import Net_Full
-from model.model_Full_2 import Net_Full_2
-from model.model_Full_3 import Net_Full_3
-from model.model_Full_4 import Net_Full_4
-from model.model_Full_final import Net_Full_final
 
-from model.model_None import Net_None
-from model.model_DPP import Net_DPP
+
 
 
 
@@ -37,25 +27,6 @@ class Test(object):
         elif cfg.net == 'Net_Full' or cfg.net == 'Full':
             net = Net_Full(cfg)
         
-        elif cfg.net == 'Net_Quant_8bit':
-            net = Net_Quant_8bit(cfg)
-        elif cfg.net == 'Net_Quant_w2bit':
-            net = Net_Quant_w2bit(cfg)
-        
-        elif cfg.net == 'Net_Full_2':
-            net = Net_Full_2(cfg)
-        elif cfg.net == 'Net_Full_3':
-            net = Net_Full_3(cfg)
-        elif cfg.net == 'Net_Full_4':
-            net = Net_Full_4(cfg)
-            
-        elif cfg.net == 'Net_Full_final':
-            net = Net_Full_final(cfg)
-            
-        elif cfg.net == 'Net_None':
-            net = Net_None(cfg)
-        elif cfg.net == 'Net_DPP':
-            net = Net_DPP(cfg)
         
         net.to(cfg.device)
         
@@ -63,10 +34,7 @@ class Test(object):
         
         filename_para = cfg.net + '_lr' + str(cfg.lr) + '_n_steps' + str(cfg.n_steps)
         model_path = '../param/' + cfg.net +'/' + filename_para + '_' + str(cfg.n_epochs) + 'best_' + cfg.best +'.pth.tar'
-        # model_path = '../param/Net_Quant_8bit/Net_Quant_8bit_lr1e-05_n_steps200_1000best_loss.pth.tar'
-        
-        # model_path = '../param/'+ cfg.net +'/' + 'Net_Full_best_mse_268.pth.tar'
-        # model_path = '../param/'+ cfg.net +'/' + 'Net_Full_best_mse_264.pth.tar'
+
         
         print('param=', model_path)
       
@@ -77,7 +45,7 @@ class Test(object):
             net = torch.nn.DataParallel(net, device_ids=[0, 1])
         
         # scene_list = ['boxes', 'cotton', 'dino', 'sideboard', 'stripes', 'pyramids', 'dots', 'backgammon']
-        scene_list = ['origami', 'bedroom', 'bicycle', 'herbs']
+        # scene_list = ['origami', 'bedroom', 'bicycle', 'herbs']
         
         scene_list = ['Black_Fence', 'Bush', 'Caution_Bees', 'Flowers', 'Gravel_Garden', 'Mirabelle_Prune_Tree', 'Palais_du_Luxembourg', 'Poppies', 'Rusty_Handle', 'Swans_1', 'Swans_2', 'Trunk', 'Vine_Wood', 'Wheat_&_Silos']
         
